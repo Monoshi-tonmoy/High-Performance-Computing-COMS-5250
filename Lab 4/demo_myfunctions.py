b65590030dd85c2c51c7d6e5972df447f5232a93
Monoshi-tonmoy <tonmoy5200@gmail.com>
2025-01-30 11:57:54 -0600

    Lab 4

---
+++ Lab 4/demo_myfunctions.py
@@ -0,0 +1,102 @@
+import math
+
+"""Square Root function using Newton's method"""
+def sqrt(number, convergence_value=1e-6, max_iterations=100):
+    if number == 0:
+        return 0
+    elif number < 0:
+        print("Please enter a valid number for square root calculation")
+        return None
+
+    s = number / 2
+
+    for i in range(max_iterations):
+        next_s = 0.5 * (s + number / s)
+        
+        if abs(next_s - s) < convergence_value:      #checking the absoulte error (Also could be done with relative error)
+            print(f"Converged after {i + 1} iterations.")
+            return next_s
+        
+        s = next_s
+        print(f"After {i + 1}th iteration the value is: {s}")
+
+    return s
+
+"""Factorial Function"""
+def fact(number):
+    if number < 0:
+        return None
+    if number == 1 or number == 0:
+        return 1
+    return number * fact(number-1)
+
+"""Exponential function using Taylor series expansion"""
+def exp(x, convergence_value=1e-6, terms=18):
+    x0 = int(round(x))
+    
+    e_x0 = math.exp(x0)
+    
+    z = x - x0
+    
+    result = e_x0
+
+    term = 1 
+    for n in range(1, terms + 1):
+        term *= z / n
+        result += term
+
+        if abs(term) < convergence_value:  # Term is defining the smallest amount of change
+            print(f"The value already converged, returning")
+            return result
+    
+    return result
+
+
+"""Logarithm Calculation"""
+def ln(x, convergence_value=1e-6, max_iterations=100):
+    if x <= 0:
+        print("Input must be a positive number.")
+        return None
+    
+    s = x
+    
+    for i in range(max_iterations):
+        exp_s = math.exp(s)
+        
+        next_s = s - (exp_s - x) / exp_s
+        
+        if abs(next_s - s) < convergence_value:
+            print(f"Converged after {i + 1} iterations.")
+            return next_s
+        
+        s = next_s
+    
+    print("Maximum iterations reached.")
+    return s
+
+
+def main():
+    value = int(input("Enter a value to calculate square root, factorial, e^value and ln(value):"))
+    print("\n................Here is the Results for Square Root function...............\n")
+    square_root_value = sqrt(value)
+    print(f"Final Square root value is {square_root_value} and math module's value is {math.sqrt(value)}")  #Comparing our defined square root function and math module's function
+    print("\n\n")
+
+    print("\n................Here is the Results for Factorial function...............\n")
+    print(f"Final factorial value is {fact(value)} and math module's value is {math.factorial(value)}") # Comaring with math modules factorial
+    print("\n\n")
+
+    print("\n................Here are the Results for the Exponential function...............\n")
+    exp_value = exp(value)
+    print(f"Final value of e^{value} is {exp_value} and math module's value is {math.exp(value)}") # Comparing with math modules exp
+    print("\n\n")
+
+    print("\n................Here are the Results for the Logarithmic function...............\n")
+    ln_value = ln(value)
+    print(f"Final value of ln({value}) is {ln_value} and math module's value is {math.log(value)}") # Comparing with math modules ln"
+    print("\n\n")
+
+
+
+if __name__ == "__main__":
+    main()
