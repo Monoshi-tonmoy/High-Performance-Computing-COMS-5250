c191eb1060e5bdcae60be098908128931b59d9d8
Monoshi Kumar Roy <tonmoy5200@gmail.com>
2025-02-04 15:34:58 -0600

    Lab 5 Updated

--- Lab 5/demo_polynomial_interpolation.py
+++ Lab 5/demo_polynomial_interpolation.py
@@ -27,12 +27,9 @@ x_plot = np.linspace(-0.15, 0.15, 100)
 f_values = np.cos(x_plot)
 p_values_plot = np.polyval(coefficients, x_plot) 
 
-print(f_values)
-print(p_values)
 
 # Plotting the cosine function graph and polynomial graph to see how they overlap
 plt.figure(figsize=(10, 4))
-plt.subplot(1, 2, 1)
 plt.plot(x_plot, f_values, label="cos(x)", linestyle="dashed", color="blue", linewidth=2)
 plt.plot(x_plot, p_values_plot, label="Interpolating Polynomial", linestyle="solid", color="red", linewidth=1)
 plt.scatter(points, values, color="black", label="Interpolation Points")
